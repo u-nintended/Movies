e5d60554bb49948bb42a7e48b08c217df07fce35
Your Name <you@example.com>
2025-11-19 09:56:01 +0300

    movies-repository

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/jkQi4WQ4LxwyHgw84ogND+Y7V2x+k+XRjDpVEag49w=\"\n}"
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello World
-    </div>
+    <div className="align-center flex-col  p-70">
+      <Button>Click me</Button>
+  
+
+      <div className="align-center flex-col  p-75"></div>
+      <Button>Click me</Button>
+      </div>
+    
+    
   );
 }
+
