e6c2cb976eb9625a07d5113164d1b471bb7b41b6
Your Name <you@example.com>
2025-11-20 11:34:09 +0300

    movies-repository

--- .next/dev/server/app/page.js
+++ .next/dev/server/app/page.js
@@ -1,6 +1,6 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
 R.c("server/chunks/ssr/node_modules_16542bb4._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__4fa85681._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__81714a3c._.js")
 R.c("server/chunks/ssr/node_modules_473cc24e._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__e8a2741f._.js")
 R.c("server/chunks/ssr/app_b9b1292a._.js")
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
+     
     <div className="align-center flex-col  p-70">
+     <Header />
+     <SideBar />
+     <NavBar />
       <Button>Click me</Button>
   
 
